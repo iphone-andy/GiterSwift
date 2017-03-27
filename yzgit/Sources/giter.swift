e53648fa716dd12fd62f26ce50c0e22a75469691
邱灿清 <1716998044@qq.com>
2017-03-27 17:23:11 +0800

    rename

---
+++ yzgit/Sources/giter.swift
@@ -0,0 +1,191 @@
+//
+//  giter.swift
+//  GitSwift
+//
+//  Created by 邱灿清 on 2017/3/24.
+//
+//
+
+import Foundation
+import PathKit
+#if os(OSX)
+    import Darwin
+#elseif os(Linux)
+    import Glibc
+#endif
+import Darwin.POSIX.stdlib
+
+public enum GitOperateType:NSInteger {
+    
+    case branch = 100
+    case commit
+    case tag
+    case checkOut
+    case checkOutTag
+    case merge
+    
+    case add = 200
+    case push
+    case pull
+    case stashSave
+    case stashPop
+    
+    public var description: String {
+        switch self {
+        case .branch:
+            return "branch"
+        case .commit:
+            return "commit"
+        case .tag:
+            return "tag"
+        case .checkOut:
+            return "checkout"
+        case .checkOutTag:
+            return "checkout"
+        case .push:
+            return "push"
+        case .pull:
+            return "pull"
+        case .merge:
+            return "merge"
+        case .add:
+            return "add"
+        case .stashSave:
+            return "stash"
+        case .stashPop:
+            return "stash"
+        }
+    }
+}
+
+public class Giter {
+    
+    private var operateType:GitOperateType
+    private var param:String
+    required public init(operateType:GitOperateType,param:String) {
+        self.operateType = operateType
+        if self.operateType.rawValue >= 200 {
+            self.param = ""
+        }else{
+            self.param = param
+        }
+    }
+    
+    public func excute() -> () {
+        
+        do {
+            var parentGit:Bool = false
+            for path:Path in try Path.current.parent().children() {
+                if path.lastComponent == ".git"{
+                    parentGit = true
+                }
+            }
+            if parentGit {
+                
+                self.excuteGit(Path.current.parent());
+                
+            }else{
+
+                for path:Path in try Path.current.parent().children() {
+
+                    if path.isDirectory {
+                        
+                        var isGit:Bool = false
+                        do {
+                            for subPath in try path.children() {
+                                if subPath.lastComponent == ".git"{
+                                    isGit = true
+                                }
+                            }
+                            if isGit {
+                                self.excuteGit(path);
+                            }
+                        } catch {
+                            
+                        }
+                    }
+                }
+            }
+        } catch {
+            
+        }
+    }
+    
+    private func excuteGit(_ path:Path)->(){
+        
+        let git = Process()
+        var gitPath = path
+        if gitPath.string.characters.count == 0 {
+            /*
+            bash.launchPath = "/usr/local/bin/bash"
+            bash.arguments = ["cd",path.string]
+            bash.launch()
+            bash.waitUntilExit()
+            print("cd \(path.string)")
+            */
+            gitPath = Path.current
+        }
+        
+        let outpipe = Pipe()
+        git.standardOutput = outpipe
+        let errpipe = Pipe()
+        git.standardError = errpipe
+        git.launchPath = "/usr/bin/git"
+        git.currentDirectoryPath = gitPath.string
+
+        var arguments:[String] = []
+        arguments.append(self.operateType.description)
+        if self.operateType == .add {
+            
+            arguments.append(".")
+            
+        }else if self.operateType == .checkOutTag {
+            
+            arguments.append("-b")
+            arguments.append("tag_\(self.param)")
+            arguments.append(self.param)
+
+        }else if self.operateType == .commit {
+            
+            if self.param.characters.count > 0 {
+                arguments.append("-m")
+                arguments.append(self.param)
+            }
+            
+        }else if self.operateType == .stashSave {
+            
+            arguments.append("save")
+
+        }else if self.operateType == .stashPop {
+
+            arguments.append("pop")
+            
+        }else{
+            if self.param.characters.count > 0 {
+                arguments.append(self.param)
+            }
+        }
+        git.arguments = arguments
+        git.launch()
+        
+        let outdata = outpipe.fileHandleForReading.availableData
+        var outputString = String(data: outdata, encoding: String.Encoding.utf8) ?? ""
+        
+        let errdata = errpipe.fileHandleForReading.availableData
+        let errString = String(data: errdata, encoding: String.Encoding.utf8) ?? ""
+        
+        git.waitUntilExit()
+        
+        if outputString.characters.count > 0 {
+            print("\(path.lastComponent) result : \(outputString)")
+        }else{
+            if errString.characters.count > 0 {
+                print("\(path.lastComponent) result : \(errString)")
+            }else{
+                outputString = "success"
+                print("\(path.lastComponent) result : \(outputString)")
+            }
+        }
+        //return outputString
+    }
+}
---
+++ yzgit/Sources/main.swift
@@ -0,0 +1,131 @@
+
+
+import Foundation
+import CommandLineKit
+import Rainbow
+
+
+let cli = CommandLineKit.CommandLine()
+
+
+let add = StringOption(shortFlag: "a", longFlag: "add",helpMessage: "添加所有变更到缓存区")
+let branch = StringOption(shortFlag: "b", longFlag: "branch",helpMessage: "新建分支")
+let tag = StringOption(shortFlag: "t", longFlag: "tag",helpMessage: "新建tag")
+let checkout = StringOption(longFlag: "co",helpMessage: "切换分支")
+let commit = StringOption(longFlag: "ci",helpMessage: "创建一个commit")
+
+let push = BoolOption(longFlag: "push",helpMessage: "提交代码")
+let pull = BoolOption(longFlag: "pull",helpMessage: "拉取代码")
+let merge = StringOption(longFlag: "merge",helpMessage: "合并代码")
+let stash_save = BoolOption(longFlag: "stash-save",helpMessage: "缓存变更")
+let stash_pop = BoolOption(longFlag: "stash-pop",helpMessage: "恢复缓存变更")
+
+//TODO:
+let prune = BoolOption(longFlag: "prune",helpMessage: "清除远程仓库已经不存在的分支")
+let checkout_tag = StringOption(longFlag: "co-tag",helpMessage: "从指定tag拉出分支并切换到该分支")//longFlag: "checkout"
+let help = StringOption(longFlag: "help",helpMessage: "查看帮助")//longFlag: "checkout"
+
+//TODO:
+// git tag -l | xargs git tag -d
+//git fetch  - - tags  清除本地tag，并拉取远程tag
+
+cli.addOptions(add,branch,tag,checkout,checkout_tag,commit,push,pull,merge,stash_save,stash_pop)
+
+cli.formatOutput = { s, type in
+    var str: String
+    switch(type) {
+    case .error:
+        str = s.red.bold
+    case .optionFlag:
+        str = s.green.underline
+    case .optionHelp:
+        str = s.blue
+    default:
+        str = s
+    }
+    
+    return cli.defaultFormat(s: str, type: type)
+}
+
+do {
+    try cli.parse()
+} catch {
+    cli.printUsage(error)
+    exit(EX_USAGE)
+}
+
+//operate
+
+if add.wasSet{
+    
+    //print("git add \(add.value)")
+    let addGiter:Giter = Giter.init(operateType: .add, param:add.value!)
+    addGiter.excute()
+    
+}else if branch.wasSet{
+    
+    //print("branch \(branch.value!)")
+    let branchGiter:Giter = Giter.init(operateType: .branch, param: branch.value!)
+    branchGiter.excute()
+    
+}else if tag.wasSet{
+    
+    //print("tag \(tag.value!)")
+    let tagGiter:Giter = Giter.init(operateType: .tag, param: tag.value!)
+    tagGiter.excute()
+    
+}else if checkout.wasSet{
+    
+    //print("checkout \(checkout.value!)")
+    let checkoutGiter:Giter = Giter.init(operateType: .checkOut, param: checkout.value!)
+    checkoutGiter.excute()
+   
+}else if checkout_tag.wasSet{
+    
+    //print("checkout_tag \(checkout_tag.value!)")
+    let checkout_tagGiter:Giter = Giter.init(operateType: .checkOut, param: checkout_tag.value!)
+    checkout_tagGiter.excute()
+    
+}else if commit.wasSet{
+    
+    //print("checkout \(checkout.value!)")
+    let commitGiter:Giter = Giter.init(operateType: .commit, param: commit.value!)
+    commitGiter.excute()
+    
+}else if push.wasSet{
+    
+    //print("push \(push.value!)")
+    let pushGiter:Giter = Giter.init(operateType: .push, param: String(push.value))
+    pushGiter.excute()
+    
+}else if pull.wasSet{
+    
+    //print("pull \(pull.value!)")
+    let pullGiter:Giter = Giter.init(operateType: .pull, param: String(pull.value))
+    pullGiter.excute()
+    
+}else if merge.wasSet{
+    
+    //print("merge \(merge.value!)")
+    let mergeGiter:Giter = Giter.init(operateType: .merge, param: merge.value!)
+    mergeGiter.excute()
+    
+}else if stash_save.wasSet{
+    
+    //print("stash_save \(stash_save.value!)")
+    let stash_saveGiter:Giter = Giter.init(operateType: .stashSave, param: String(stash_save.value))
+    stash_saveGiter.excute()
+    
+}else if stash_pop.wasSet{
+    
+    //print("stash_pop \(stash_pop.value!)")
+    let stash_popGiter:Giter = Giter.init(operateType: .stashPop, param: String(stash_pop.value))
+    stash_popGiter.excute()
+    
+}else{
+    
+    print(cli.printUsage())
+    
+}
+
+
